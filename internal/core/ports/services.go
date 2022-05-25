4e46fa97dab5a268478f866f79db72c7ed91dfad
viswamvs <viswavickymvs@gmail.com>
2022-05-25 18:16:31 +0530

    Added services for city, currency, country, hs code, ports

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -4,74 +4,110 @@ import "wiz-liners/internal/core/domain/services"
 
 type CitiesService interface{
 	Create(
+		Name string,
+		Country_Id int64,
+	)(services.Cities,error)
+	
+	Get(
+		id int64,
+	)(services.Cities, error)
 
-		)()
-		GetMany(
-	
-		)()
-		Get(
-	
-		)()
-		Modify(
-	
-		)()
-		Remove(
-	
-		)()
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Cities, error)
+
+	Modify(
+		id int64,
+		Name *string,
+		Country_Id *int64,
+	)(services.Cities, error)
+
+	Remove(
+		id int64,
+	)(error)
 }
 type CountriesService interface{
 	Create(
+		Name string,
+		Iso_code string,
+		Currency_code string,
+	)(services.Countries, error)
 
-		)()
-		GetMany(
-	
-		)()
-		Get(
-	
-		)()
-		Modify(
-	
-		)()
-		Remove(
-	
-		)()
+	Get(
+		id int64,
+	)(services.Countries, error)
+
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Countries, error)
+
+	Modify(
+		id int64,
+		Name *string,
+		Iso_code *string,
+		Currency_code *string,
+	)(services.Countries, error)
+
+	Remove(
+		id int64,
+	)(error)
 	
 }
 type CurrenciesService interface{
 	Create(
+		Code string,
+		Name string,
+	)(services.Currencies, error)
 
-		)()
-		GetMany(
-	
-		)()
-		Get(
-	
-		)()
-		Modify(
-	
-		)()
-		Remove(
-	
-		)()
+	Get(
+		code string,
+	)(services.Currencies, error)
+
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Currencies, error)
+
+	Modify(
+		code string,
+		Name *string,
+	)(services.Currencies, error)
+
+	Remove(
+		code string,
+	)(error)
 	
 }
 type Hs_CodeService interface{
 	Create(
+		Code string,
+		Name string,
+		Description string,
+		Parent_code string,
+	)(services.Hs_codes, error)
 
-		)()
-		GetMany(
-	
-		)()
-		Get(
-	
-		)()
-		Modify(
-	
-		)()
-		Remove(
-	
-		)()
-	
+	Get(
+		code string,
+	)(services.Hs_codes, error)
+
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Hs_codes, error)
+
+	Modify(
+		code string,
+		Name *string,
+		Description *string,
+		Parent_code *string,
+	)(services.Hs_codes, error)
+
+	Remove(
+		code string,
+	)(error)
+
 }
 type LinersService interface{
 	Create(
@@ -79,45 +115,62 @@ type LinersService interface{
 		Code string,
 		Type string,
 		Logo string,
-		)(services.Liners,error)
+	)(services.Liners,error)
 	
-		Get(
-			id int64,
-		)(services.Liners,error)
+	Get(
+		id int64,
+	)(services.Liners,error)
 		
-		GetMany(
-			pageNumber *uint,
-			itemsPerPage uint,
-		)([]services.Liners,error)
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Liners,error)
 	
-		Modify(
-			id int64,
-			Name *string,
-			Code *string,
-			Type *string,
-			Logo *string,
-		)(services.Liners,error)
+	Modify(
+		id int64,
+		Name *string,
+		Code *string,
+		Type *string,
+		Logo *string,
+	)(services.Liners,error)
 	
-		remove(
-			id int64,
-		)(error)
+	Remove(
+		id int64,
+	)(error)
 	
 }
 type PortsService interface{
 	Create(
+		Code string,
+		Name string,
+		Type string,
+		City_Id int64,
+		State string,
+		Latitude float64,
+		Longitude float64,
+	)(services.Port, error)
+	
+	Get(
+		code string,
+	)(services.Port, error)
 
-		)()
-		GetMany(
-	
-		)()
-		Get(
-	
-		)()
-		Modify(
-	
-		)()
-		Remove(
-	
-		)()
+	GetMany(
+		pageNumber *uint,
+		itemsPerPage uint,
+	)([]services.Port, error)
+
+	Modify(
+		code string,
+		Name *string,
+		Type *string,
+		City_Id *int64,
+		State *string,
+		Latitude *float64,
+		Longitude *float64,
+	)(services.Port, error)
+
+	Remove(
+		code string,
+	)(error)
 	
 }
