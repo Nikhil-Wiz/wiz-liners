fcee4082ad643e35c15b3000ddde3ec21afb091e
viswamvs <viswavickymvs@gmail.com>
2022-05-30 11:35:45 +0530

    Added migrations

--- go.mod
+++ go.mod
@@ -7,7 +7,11 @@ require (
 	github.com/samber/lo v1.21.0
 )
 
+require github.com/pkg/errors v0.9.1 // indirect
+
 require (
 	github.com/doug-martin/goqu/v9 v9.18.0
+	github.com/lib/pq v1.10.6
+	github.com/pressly/goose/v3 v3.5.3
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 )
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"embed"
+
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
+func main() {
+
+	var conninfo string = "host=localhost port=55000 user=postgres password=postgrespw dbname=wiz sslmode=disable"
+	db, err := sql.Open("postgres", conninfo)
+	if err != nil {
+		panic(err)
+	}
+
+	// setup database
+	goose.SetBaseFS(embedMigrations)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		panic(err)
+	}
+
+	if err := goose.Up(db, "migrations"); err != nil {
+		panic(err)
+	}
+
+	// run app
+}
