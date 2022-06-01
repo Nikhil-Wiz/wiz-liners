9796b1c446fe4b4f476f12beda550bca5af8766c
viswamvs <viswavickymvs@gmail.com>
2022-06-01 15:53:00 +0530

    Added triggers for 'modified_at' field

--- migrations/20220530103550_hs_codes.sql
+++ migrations/20220530103550_hs_codes.sql
@@ -1,14 +1,29 @@
 -- +goose Up
 -- +goose StatementBegin
+
+
 CREATE TABLE IF NOT EXISTS hs_codes (
     code VARCHAR(255) NOT NULL,
     name VARCHAR(255),
     description VARCHAR(255),
     parent_code VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
     PRIMARY KEY(code)
 );
+
+CREATE OR REPLACE FUNCTION hscodes_trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON hs_codes
+FOR EACH ROW
+EXECUTE PROCEDURE hscodes_trigger_set_timestamp();
 -- +goose StatementEnd
 
 -- +goose Down
--- migrations/20220530103751_countries.sql
+++ migrations/20220530103751_countries.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
+
 CREATE TABLE IF NOT EXISTS currencies (
     code VARCHAR(255) NOT NULL,
     name VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
     PRIMARY KEY(code)
 );
 
@@ -13,8 +14,8 @@ CREATE TABLE IF NOT EXISTS countries (
     name VARCHAR(255),
     iso_code VARCHAR(32),
     currency_code VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
     PRIMARY KEY(id),
     CONSTRAINT idx_currency_code FOREIGN KEY(currency_code) REFERENCES currencies(code)
 );
@@ -23,11 +24,51 @@ CREATE TABLE IF NOT EXISTS cities (
     id SERIAL,
     name VARCHAR(255),
     country_id int,
-    created_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMP NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
     PRIMARY KEY(id),
     CONSTRAINT idx_cities FOREIGN KEY(country_id) REFERENCES countries(id)
 );
+
+CREATE OR REPLACE FUNCTION currencies_trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON currencies
+FOR EACH ROW
+EXECUTE PROCEDURE currencies_trigger_set_timestamp();
+
+CREATE OR REPLACE FUNCTION countries_trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON countries
+FOR EACH ROW
+EXECUTE PROCEDURE countries_trigger_set_timestamp();
+
+CREATE OR REPLACE FUNCTION cities_trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.modified_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON cities
+FOR EACH ROW
+EXECUTE PROCEDURE cities_trigger_set_timestamp();
+
 -- +goose StatementEnd
 
 -- +goose Down
